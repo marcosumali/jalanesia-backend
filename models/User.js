2f6a06212340e00ec8b68f4cc23b306639d02ec0
marcosumali <marco.sumali90@gmail.com>
2018-05-13 19:31:57 +0700

    add middlewares for user authentication, update controllers methods: signin & register with jwt, delete, get user profile

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 const bcrypt = require('bcrypt')
+
+const Schema = mongoose.Schema
 const saltRounds = 10
 
 const userSchema = new Schema({
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const { findAll, findOne, add, deletion, signIn, getProfile, deleteUser } = require('../controllers/user.controller')
+const { authentication, authorisation } = require('../middlewares/auth')
+
 const users = express.Router()
-const { findAll, findOne, add, deletion } = require('../controllers/user.controller')
 
 // need to add update, login, logout, addComment, fblogin, g+ login, twitter login
 
 users
-  .post('/', add)
-  .get('/', findAll)
-  .get('/:id', findOne)
-  .delete('/:id', deletion)
+  .get('/', findAll)                                                          // GET all users (admin role only) - admin not yet prepared
+  // .get('/:id', findOne)                                                    // redundant with getProfile
+  .get('/profile', authentication, authorisation, getProfile)                 // GET user profile (admin role and related user only)
+  .post('/', add)                                                             // POST new user (register)
+  .post('/signin', signIn)                                                    // POST user verification to get token (sign in)
+  // .delete('/:id', deletion)                                                // redundant with delete
+  .delete('/delete', authentication, authorisation, deleteUser)               // DELETE user profile
 
 module.exports = users
